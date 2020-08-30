f5ef7ccbb313e38f34053b74b9b9735739d2d7e0
Ralph F <RaftsDev@gmail.com>
2020-08-30 10:35:41 -0400

    Created fields of account class

---
+++ src/com/company/Account.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Account {
+    private String name;
+    private int number;
+    private String mail;
+    private String phone;
+    private double balance;
+    private double deposit;
+    private double withdraw;
+}
