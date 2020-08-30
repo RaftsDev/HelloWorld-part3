97aeddb71279f5b88e39bbfa9e1293f1d06bf5e8
Ralph F <RaftsDev@gmail.com>
2020-08-30 13:40:59 -0400

    main: Added method deposit and withdraw.

--- src/com/company/Account.java
+++ src/com/company/Account.java
@@ -9,7 +9,7 @@ public class Account {
     private double deposit;
     private double withdraw;
 
-//    Major constructor
+    //    Major constructor
     public Account(String name, int number, String mail, String phone, double balance) {
         this.name = name;
         this.number = number;
@@ -20,8 +20,13 @@ public class Account {
     }
 
     public Account(String name, int number, String mail, String phone) {
-     this(name,number,mail,phone,50.00);
+        this(name, number, mail, phone, 50.00);
         System.out.println("4 params constr called");
+    }
+
+    public Account(String name, int number) {
+        this(name, number, "defaultMail", "-", 50.00);
+        System.out.println("2 params constr called");
     }
 
     public String getName() {
@@ -36,8 +41,19 @@ public class Account {
         return balance;
     }
 
-    public Account(String name, int number) {
-        this(name,number,"defaultMail","-",50.00);
-        System.out.println("2 params constr called");
+    public void deposit(double deposit) {
+        System.out.println("Balance before deposit:"+this.balance);
+        this.balance+=deposit;
+        System.out.println("Balance after deposit:"+this.balance);
+    }
+
+    public void withdraw(double withdraw){
+        if(this.balance-withdraw<0){
+            System.out.println("Not enogph on balance");
+        }else{
+            System.out.println("Balance before withdraw:"+this.balance);
+            this.balance-=withdraw;
+            System.out.println("Balance after withdraw:"+this.balance);
+        }
     }
 }
