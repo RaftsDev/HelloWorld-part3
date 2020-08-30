a7e88e0ad0b512e5d46e2bc6dc3ef6b0070ca332
Ralph F <RaftsDev@gmail.com>
2020-08-30 12:08:08 -0400

    major and 2 overloading constructors.

--- src/com/company/Account.java
+++ src/com/company/Account.java
@@ -9,19 +9,23 @@ public class Account {
     private double deposit;
     private double withdraw;
 
+//    Major constructor
     public Account(String name, int number, String mail, String phone, double balance) {
         this.name = name;
         this.number = number;
         this.mail = mail;
         this.phone = phone;
         this.balance = balance;
+        System.out.println("Major constructor called");
     }
 
     public Account(String name, int number, String mail, String phone) {
-        this.name = name;
-        this.number = number;
-        this.mail = mail;
-        this.phone = phone;
+     this(name,number,mail,phone,50.00);
+        System.out.println("4 params constr called");
+    }
 
+    public Account(String name, int number) {
+        this(name,number,"defaultMail","-",50.00);
+        System.out.println("2 params constr called");
     }
 }
