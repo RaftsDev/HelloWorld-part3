fa5785599975acdcd0e106ea9147d75b49c7a7b2
Ralph F <RaftsDev@gmail.com>
2020-08-31 09:24:20 -0400

    main: created class Animal

---
+++ src/com/company/Animal.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Animal {
+    String name;
+    int size;
+    int weight;
+
+
+    public void eat(){
+        System.out.println("Animal eat");
+    }
+
+    public void move(double speed){
+        System.out.println("Animal move in speed: "+speed);
+    }
+}
+
